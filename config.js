44c6b7627d9aa2edee05c8273d6fd8ffaefbec01
aakanskha <aakankshasingh014@gmail.com>
2017-08-27 23:08:27 +0530

    push changed files

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-		consumer_key:         'vHw4Cj4ve8vq59fzKneZwLrQc',
-		consumer_secret:      'zqJMquttS1ntzYl1AvlF5bFEJG1GS4UGo5exXEj1pDx5IgbsQW',
-		access_token:         '893019741169696768-v4F7KSuztq3I0S0tZcfzyvMSDyqy80X',
-		access_token_secret:  'PvJVRPU4NxmRUOq0FADB6IXBgak0R5emSUY3GUd3hsYRc',
+		consumer_key:         '', //Enter your consumer_key
+		consumer_secret:      '', //Enter your consumer_secret
+		access_token:         '', //Enter your access_token
+		access_token_secret:  '', //Enter your token_secret 
 		timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
